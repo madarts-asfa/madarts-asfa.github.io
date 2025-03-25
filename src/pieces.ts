ddbd8a70e088211c4b4c96a9d80531cfc86a8414
Alex S <bobylito@users.noreply.github.com>
2025-03-25 23:51:13 +0200

    fixes images of art pieces

--- src/pieces.ts
+++ src/pieces.ts
@@ -1,6 +1,6 @@
 export type Piece = [string, string, string];
 export const pieces: Piece[] = [
-  ["Polychronopoulos Ioannis", "Shifting Realities", "polychronopoulos3.jpg"],
+  ["Polychronopoulos Ioannis", "Shifting Realities", "polychronopoulos3.png"],
   ["Polychronopoulos Ioannis", "Fragmented Movements", "polychronopoulos.png"],
   ["Rafail Tsakmakis", "1.ShapeHole Symphony", "1_Rafail_Tsakmakis.png"],
   ["Rafail Tsakmakis", "2.Cosmic Tunel", "2_Rafail_Tsakmakis.png"],
@@ -22,33 +22,33 @@ export const pieces: Piece[] = [
   [
     "Gina Stavropoulou",
     '"Endless Inter-Connection"',
-    "1_Gina Stavropoulou_Endless Inter - Connection",
+    "1_Gina Stavropoulou_Endless Inter - Connection.png",
   ],
   [
     "Gina Stavropoulou",
     '"Scratch Desire"',
-    "2_Gina Stavropoulou_Scratch Desire",
+    "2_Gina Stavropoulou_Scratch Desire.png",
   ],
   [
     "Gina Stavropoulou",
     '"Techno-Organic"',
-    "3_Gina Stavropoulou_Techno Organic",
+    "3_Gina Stavropoulou_Techno Organic.png",
   ],
   ["Semina Milarokosta ", " Avatar", ""],
-  ["Stergios Samsakis", "Eternal Creation", ""],
-  ["Stergios Samsakis", "Voice Circle", ""],
-  ["Stergios Samsakis", "Camera Illusion", ""],
+  ["Stergios Samsakis", "Eternal Creation", "1.Stergios Samsakis.png"],
+  ["Stergios Samsakis", "Voice Circle", "2.Strgios Samsakis.png"],
+  ["Stergios Samsakis", "Camera Illusion", "3. Stergios Samsakis.png"],
   [
     "Konstantina Boua",
     '"Resonant Dunes" – An Audio-Driven Digital Landscape',
-    "resonantDunes_Boua_Konstantina",
+    "1.resonantDunes_Boua_Konstantina.png",
   ],
   [
     "Konstantina Boua",
     'Particles of Memory – An Audiovisual Flow"',
-    "particlesMemory_Boua_Konstantina",
+    "particlesMemory_Boua_Konstantina.png",
   ],
-  ["Elena Anagnostaki", "Pixelworks", "Elena Anagnostaki Pixelworks"],
+  ["Elena Anagnostaki", "Pixelworks", "Elena Anagnostaki Pixelworks.jpg"],
   [
     "Mihalis Gkatzogias",
     "Sisyphus little keyboard piano ",
