939e765a0920cfc71d49c3cc1dd24d58a2d2e181
Alex S <bobylito@users.noreply.github.com>
2025-04-02 23:51:41 +0300

    fix: uses local ML models

--- public/art/7/sketch.js
+++ public/art/7/sketch.js
@@ -216,6 +216,13 @@ function setup() {
     withExpressions: true,
     withDescriptors: true,
     minConfidence: 0.5,
+    Mobilenetv1Model: `http://${document.location.host}/ssd_mobilenetv1_model-weights_manifest.json`,
+    //Mobilenetv1Model:
+    // "https://raw.githubusercontent.com/ml5js/ml5-data-and-models/face-api/models/faceapi/ssd_mobilenetv1_model-weights_manifest.json",
+    FaceLandmarkModel: `http://${document.location.host}/face_landmark_68_model-weights_manifest.json`,
+    FaceLandmark68TinyNet: `http://${document.location.host}/face_landmark_68_tiny_model-weights_manifest.json`,
+    FaceRecognitionModel: `http://${document.location.host}/face_recognition_model-weights_manifest.json`,
+    FaceExpressionModel: `http://${document.location.host}/face_expression_model-weights_manifest.json`,
   };
 
   faceApi = ml5.faceApi(video, faceOptions, faceReady);
