5f925517c162cec1aaa25a54cb593c4049065a78
Alex S <bobylito@users.noreply.github.com>
2025-04-05 03:06:50 +0300

    fix: adds about page resolution in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import type { UserConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
   // ...
   // base: "/psimiti-kiosk/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
 } satisfies UserConfig;
